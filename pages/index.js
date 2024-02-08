7e951d4afd27f6a61c5b49a0afee0db2104711b0
Ben Rehmann <brehmann@me.com>
2024-02-08 14:06:10 -0600

    update

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,74 @@ const home = () => {
             <div className="page-div">
                 <h1>Home</h1>
                 {/* <FontAwesomeIcon icon="fa-solid fa-dog" /> */}
+
+                <div className='filmContainer'>
+
+                    <div className="filmStrip">
+
+                        <div className='filmHoles'>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+
+                        <div className='filmSlides'>
+                            <div className='slide'></div>
+                            <div className='slide'></div>
+                            <div className='slide'></div>
+                            <div className='slide'></div>
+                            <div className='slide'></div>
+                            <div className='slide'></div>
+                        </div>
+
+                        <div className='filmHoles'>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+
+                    </div>
+                </div>
+
+
             </div>
         </Page>
     )
